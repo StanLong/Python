e2bf98ecb0bb7c071c9ec2f37fbfdb3ebf2c5c97
StanLong <youlinbhz@sina.cn>
2020-12-30 22:27:56 +0800

    构建请求头

--- 爬虫/构建请求头.py
+++ 爬虫/构建请求头.py
@@ -8,7 +8,7 @@ url = 'http://www.baidu.com/'
 
 # 构造伪装的头部
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
 }
 
 # 构造请求的对象
