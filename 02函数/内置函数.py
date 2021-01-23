fc1f09ab2ddb44fdf2446de2cfcb6c7c3a3caafc
StanLong <youlinbhz@sina.cn>
2021-01-23 22:39:32 +0800

    内置函数

---
+++ 02函数/内置函数.py
@@ -0,0 +1,54 @@
+# eval ： 执行字符串类型的代码，并返回最终结果
+# s = input('输入a+b:')
+# print(eval(s)) # eval 可以动态的执行代码，代码必须有返回值
+
+# exec: 执行字符串类型的代码，不返回任何内容
+# s = "25*4"
+# a = exec(s) # exec 不返回任何内容
+# print(a)
+# None
+# exec 执行函数
+# exec("""
+# def func():
+#     print("贵出如粪土，贱取如珠玉")
+#
+# func()
+# """)
+
+# compile 将字符串代码编译，代码对象能够通过exec来执行或者通过eval进行求值
+# 参数说明
+# 1. resource 要执行的代码，动态代码片段
+# 2. 文件名，代码存放的文件名，当传入了第一个参数的时候，这个参数给空就可以了
+# 3. 模式 取值有三个
+#   1. exec: 一般放一些流程语句的时候
+#   2. eval：resource 只存放一个求值表达式
+#   3. single： resource 存放的代码有交互的时候，mode应为single
+# code1 = "for i in range(10):print(i)"
+# com = compile(code1, "", mode="exec") # compile 只是编译，不会执行代码
+# exec(com)
+
+# code2 = "5+6"
+# com = compile(code2, "", mode="eval")
+# print(eval(com))
+
+# code3 = "name=input('请输入沈万三的身份：')"
+# com = compile(code3, "", mode="single")
+# exec(code3)
+# print(name)
+
+# repr 原样输出
+# name = "沈万三\n明朝人"
+# print(repr(name))
+# '沈万三\n明朝人'
+
+# ord 返回字符在编码表中的码位
+# print(ord('a'))
+# 97
+
+# chr 返回码位对应的字符
+# print(chr(97))
+# a
+
+# format 与具体数据有关，用于计算各种小数，精算等
+print(format(10, 'b')) # 二进制
+# 1010
