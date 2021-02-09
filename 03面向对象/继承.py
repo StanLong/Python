a88b8c72ddfb6924901e3de56b7adf50da3cfc6f
StanLong <youlinbhz@sina.cn>
2021-02-09 21:19:20 +0800

    多重继承

--- 03面向对象/继承.py
+++ 03面向对象/继承.py
@@ -1,33 +1,57 @@
 # 继承：子类获得了父类的全部属性及功能
 
-class Person(object):
-    def __init__(self,name,sex):
-        self.name = name
-        self.sex = sex
+# class Person(object):
+#     def __init__(self,name,sex):
+#         self.name = name
+#         self.sex = sex
+#
+#     def print_title(self):
+#         if self.sex == "male":
+#             print("man")
+#         elif self.sex == "female":
+#             print("woman")
+#
+# class Child(Person):         # Child 继承 Person
+#     # def __init__(self, name, sex, age): # 子类自定义初始化方法
+#     #     self.name = name
+#     #     self.sex = sex
+#     #     self.age = age
+#
+#     def __init__(self, name, sex, age):
+#         #Person.__init__(self,name,sex) # 使用父类的初始化方法 可用super关键字代替
+#         super().__init__(name, sex)
+#         self.age = age
+#     def print_title(self):   # 当子类和父类都存在相同的 print_title()方法时，子类的 print_title() 覆盖了父类的 print_title()，在代码运行时，会调用子类的 print_title()这样，这就是继承的另一个好处：多态
+#         print("子类")
+#
+#
+# May = Child("May","female",18)
+# Peter = Person("Peter","male")
+#
+# print(May.name,May.sex, May.age, Peter.name,Peter.sex)    # 子类继承父类方法及属性
+# May.print_title()
+# Peter.print_title()
 
-    def print_title(self):
-        if self.sex == "male":
-            print("man")
-        elif self.sex == "female":
-            print("woman")
+# 多重继承
+# python 3新式类，python2 经典类
+# 新式类是在创建的时候继承内置object对象
+# 新式类的MRO(Method Resolution Order， 方法解析顺序)，采用的是从左到右，广度优先的方式进行查找。
+# 比如下面的代码，类A即为新式类，查找顺序为D->B->C->A，因此print D().name()调用的是类C的name方法，将输出字符C
 
-class Child(Person):         # Child 继承 Person
-    # def __init__(self, name, sex, age): # 子类自定义初始化方法
-    #     self.name = name
-    #     self.sex = sex
-    #     self.age = age
+class A(object):
+    def name(self):
+        return 'A'
 
-    def __init__(self, name, sex, age):
-        #Person.__init__(self,name,sex) # 使用父类的初始化方法 可用super关键字代替
-        super().__init__(name, sex)
-        self.age = age
-    def print_title(self):   # 当子类和父类都存在相同的 print_title()方法时，子类的 print_title() 覆盖了父类的 print_title()，在代码运行时，会调用子类的 print_title()这样，这就是继承的另一个好处：多态
-        print("子类")
+class B(A):
+    pass
 
+class C(A):
+    def name(self):
+        return 'C'
 
-May = Child("May","female",18)
-Peter = Person("Peter","male")
+class D(B, C):
+    pass
 
-print(May.name,May.sex, May.age, Peter.name,Peter.sex)    # 子类继承父类方法及属性
-May.print_title()
-Peter.print_title()
+d = D()
+print(d.name())
+print(D.mro()) # 打印类的继承关系
