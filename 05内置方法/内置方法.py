bdfcef63eba30ddf4d29297e25b8bdf6cfbdfda3
StanLong <youlinbhz@sina.cn>
2021-02-11 21:31:40 +0800

    内置方法

--- 05内置方法/内置方法.py
+++ 05内置方法/内置方法.py
@@ -1,12 +1,12 @@
 # 内置方法
 # __call__
-class A:
-    def __call__(self, *args, **kwargs):
-        print("执行__call__方法")
-
-A()() # 相当于调用了 __call__ 方法
-a = A()
-a()    # 相当于调用了 __call__ 方法
+# class A:
+#     def __call__(self, *args, **kwargs):
+#         print("执行__call__方法")
+#
+# A()() # 相当于调用了 __call__ 方法
+# a = A()
+# a()    # 相当于调用了 __call__ 方法
 
 
 # __new__ 构造方法
@@ -32,7 +32,7 @@ class Mylist:
 mylist = Mylist()
 print(len(mylist)) # 打印对象的长度，相当于执行了类中的 __len__ 方法
 
-## __str__ / __repr__
+# __str__
 class Student:
     def __str__(self):
         return '%s %d %s'%(self.name, self.age, self.job)
@@ -43,3 +43,20 @@ class Student:
 student = Student("沈万三", 30, "商人")
 print(student.name, student.age, student.job) # 打印一个对象的全部属性，如果不想这么麻烦可以实现 __str__ 方法
 print(student) # 实现 __str__ 后再打印对象，会直接打印出对象的全部属性
+
+# __repr__
+# __repr__ 是 __str__ 的备用方法，如果有__str__方法，那么print('%s' %str) 都先执行__str__的方法
+a = '123'
+print(a) # 123
+print(repr(a)) # '123'
+
+class A:
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return '*%s*' %self.name
+    def __repr__(self):
+        return self.name
+a = A('StanLong')
+print(a) # 有 __str__ 则先打印 *StanLong*，否则打印 StanLong
+
