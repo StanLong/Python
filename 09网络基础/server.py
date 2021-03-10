9e93987f98df2bdfe4f9ed9320060df9dccd430a
StanLong <youlinbhz@sina.cn>
2021-03-10 22:26:37 +0800

    socket udp协议

--- 09网络基础/server.py
+++ 09网络基础/server.py
@@ -1,3 +1,4 @@
+# TCP协议
 import socket
 sk = socket.socket()
 sk.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # 允许连接地址重用，避免重启时报 address already in user
@@ -22,4 +23,19 @@ while True:
 
 # 关闭连接
 conn.close()
+sk.close()
+
+# UDP协议
+# udp的server不需要进行监听，也不需要建立长连接
+# 在启动服务之后，只能被动的等待客户端发送信息
+# 恢复消息的时候，除了要发送信息， 还需要把对方的地址发过去
+import socket
+sk = socket.socket(type=socket.SOCK_DGRAM) # udp协议需要指定一个 type=socket.SOCK_DGRAM
+ip_port = ('127.0.0.1', 8080)
+sk.bind(ip_port)
+
+msg, addr = sk.recvfrom(1024)
+print(msg.decode('utf-8'))
+sk.sendto(b'Server', addr)
+
 sk.close()
