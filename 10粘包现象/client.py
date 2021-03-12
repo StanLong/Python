d74a520826d2154db4059c7c74b640cd8d0c4c71
StanLong <youlinbhz@sina.cn>
2021-03-12 21:34:15 +0800

    解决黏包的方式一

--- 10粘包现象/client.py
+++ 10粘包现象/client.py
@@ -21,31 +21,57 @@
 
 # 基于UDP实现远程执行命令
 
+# import socket
+# import subprocess
+#
+# sk = socket.socket(type=socket.SOCK_DGRAM)
+# ip_port = ('127.0.0.1', 8080)
+#
+# sk.sendto('Hello'.encode('utf-8'), ip_port)
+#
+# while True:
+#     # cmd = sk.recv(10240).decode('utf-8')
+#     cmd, addr = sk.recvfrom(1024)
+#     cmd = cmd.decode('utf-8')
+#     print(cmd)
+#     result = subprocess.Popen(cmd,
+#                               shell=True,
+#                               stdout=subprocess.PIPE,
+#                               stderr= subprocess.PIPE)
+#
+#     # sk.send(result.stdout.read())
+#     # sk.send(result.stderr.read())
+#     std_err = 'stderr :' + (result.stderr.read()).decode('gbk')
+#     std_out = 'stdout :' + (result.stdout.read()).decode('gbk')
+#     if len(std_out) == 8:
+#         sk.sendto(std_err.encode('utf-8'), ip_port)
+#     else:
+#         sk.sendto(std_out.encode('utf-8'), ip_port)
+#
+# sk.close()
+
+
+# 解决黏包方式一
+# 客户端直接返回信息的长度
+
 import socket
 import subprocess
-
-sk = socket.socket(type=socket.SOCK_DGRAM)
+sk = socket.socket()
 ip_port = ('127.0.0.1', 8080)
-
-sk.sendto('Hello'.encode('utf-8'), ip_port)
+sk.connect(ip_port)
 
 while True:
-    # cmd = sk.recv(10240).decode('utf-8')
-    cmd, addr = sk.recvfrom(1024)
-    cmd = cmd.decode('utf-8')
-    print(cmd)
+    cmd = sk.recv(1024).decode('gbk')
+
     result = subprocess.Popen(cmd,
                               shell=True,
                               stdout=subprocess.PIPE,
                               stderr= subprocess.PIPE)
 
-    # sk.send(result.stdout.read())
-    # sk.send(result.stderr.read())
-    std_err = 'stderr :' + (result.stderr.read()).decode('gbk')
-    std_out = 'stdout :' + (result.stdout.read()).decode('gbk')
-    if len(std_out) == 8:
-        sk.sendto(std_err.encode('utf-8'), ip_port)
-    else:
-        sk.sendto(std_out.encode('utf-8'), ip_port)
+    std_out = result.stdout.read()
+    std_err = result.stderr.read()
+    sk.send(str(len(std_out) + len(std_err)).encode('utf-8'))
+    sk.send(std_out)
+    sk.send(std_err)
 
 sk.close()
--- 10粘包现象/server.py
+++ 10粘包现象/server.py
@@ -1,6 +1,10 @@
 # 客户端发送hello,如果服务端 recv(1) ,那只能接收到 h 这一个字符，然后再recv(1) 一下，可以再接收一个 e ，
 # 因为客户端发送的结果长，所以只能把其他的先缓存下来，下次recv的时候再去接收。
 # 这就是粘包，即两次结果粘到一起了。
+# 多次连续send小数据，也会发生黏包现象。
+# 同样send一个大数据，连续 recv 小数据也会发生粘包现象
+
+# 发生黏包现象的本质是不知道接收或者发送的数据有多大
 
 # 基于TCP实现远程执行命令
 # server端下发命令
@@ -27,19 +31,42 @@
 
 # 基于UDP实现远程执行命令
 
-import socket
-sk = socket.socket(type=socket.SOCK_DGRAM)
-ip_port = ('127.0.0.1', 8080)
-sk.bind(ip_port)
-
-msg, addr = sk.recvfrom(1024)
-print(msg.decode('utf-8'))
-while True:
-    cmd = input('>>')
-    sk.sendto(cmd.encode('utf-8'), addr)
-    msg, addr = sk.recvfrom(10240)
-    print(msg.decode('utf-8'))
-
-sk.close()
+# import socket
+# sk = socket.socket(type=socket.SOCK_DGRAM)
+# ip_port = ('127.0.0.1', 8080)
+# sk.bind(ip_port)
+#
+# msg, addr = sk.recvfrom(1024)
+# print(msg.decode('utf-8'))
+# while True:
+#     cmd = input('>>')
+#     sk.sendto(cmd.encode('utf-8'), addr)
+#     msg, addr = sk.recvfrom(10240)
+#     print(msg.decode('utf-8'))
+#
+# sk.close()
 
 # UDP 协议不黏包，但是会丢包
+
+
+# 解决黏包方式一
+# 客户端直接返回信息的长度
+# 优点: 确定了接收多大的数据
+# 缺点：多了一次交互
+import socket
+sk = socket.socket()
+ip_port = ('127.0.0.1', 8080)
+sk.bind(ip_port)
+sk.listen()
+
+conn, addr = sk.accept()
+
+while True:
+    cmd = input('>>')
+    conn.send(cmd.encode('gbk'))
+    num = conn.recv(1024).decode('utf-8')
+    msg = conn.recv(int(num)).decode('gbk')
+    print(msg)
+
+conn.close()
+sk.close()
