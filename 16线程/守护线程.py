2828bc49f837a31b7ab980739967b7e267f4634f
StanLong <youlinbhz@sina.cn>
2021-04-18 20:48:21 +0800

    守护线程

---
+++ 16线程/守护线程.py
@@ -0,0 +1,16 @@
+# 守护线程
+
+import time
+import threading
+def func():
+    print("开始执行子进程 %s" %threading.currentThread().getName())
+    time.sleep(1)
+    print("子进程执行结束 %s" %threading.currentThread().getName())
+
+t1 = threading.Thread(target=func)
+t1.setDaemon(True)
+t1.start()
+
+t2 = threading.Thread(target=func)
+t2.start()
+t2.join()
