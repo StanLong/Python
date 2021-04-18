4acb689a9a99be448c1a1a3d08d80b51e7863b68
StanLong <youlinbhz@sina.cn>
2021-04-18 20:16:07 +0800

    线程

--- 16线程/线程.py
+++ 16线程/线程.py
@@ -36,25 +36,39 @@
 # print(t.count)
 
 # 传参
-from threading import Thread
+# from threading import Thread
+# import time
+# import os
+#
+# class MyThread(Thread):
+#     count = 0
+#     def __init__(self, arg1, arg2):
+#         super().__init__()
+#         self.arg1 = arg1
+#         self.arg2 = arg2
+#
+#     def run(self) -> None:
+#         MyThread.count = MyThread.count + 1
+#         time.sleep(1)
+#         # print('Hello word %s' %os.getpid())
+#         print('%s, %s, %s, %s'%(self.arg1, self.arg2, self.name, os.getpid()))
+#
+# for i in range(10):
+#     t = MyThread(i, (i+1) * '*')
+#     t.start()
+#     t.join()
+
+# 多个子线程共享数据
+import threading
 import time
-import os
 
-class MyThread(Thread):
-    count = 0
-    def __init__(self, arg1, arg2):
-        super().__init__()
-        self.arg1 = arg1
-        self.arg2 = arg2
-
-    def run(self) -> None:
-        MyThread.count = MyThread.count + 1
-        time.sleep(1)
-        # print('Hello word %s' %os.getpid())
-        print('%s, %s, hello world, %s'%(self.arg1, self.arg2, os.getpid()))
+def func(i):
+    time.sleep(1)
+    print(i, threading.currentThread().getName(), threading.currentThread().ident) # ident 线程id
 
 for i in range(10):
-    t = MyThread(i, (i+1) * '*')
+    t = threading.Thread(target=func, args=(i,))
     t.start()
-    t.join()
 
+# print(threading.enumerate()) # 返回正在运行着的线程列表
+print(threading.activeCount()) # 正在运行着的线程个数
