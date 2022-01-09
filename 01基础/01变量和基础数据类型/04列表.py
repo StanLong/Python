801ab8f9a1febf0568c8687fd282bd80bf43335b
StanLong <youlinbhz@sina.cn>
2022-01-09 17:34:02 +0800

    二维列表

--- 01基础/01变量和基础数据类型/04列表.py
+++ 01基础/01变量和基础数据类型/04列表.py
@@ -8,3 +8,14 @@ lst = [1,2,3]
 lst.push(4) # 入栈
 lst.pop() # 弹栈
 lst[-1] # 返回栈顶元素
+
+# 定义二维数组
+# 方式一:
+chessArr1 = []
+for i in range(11):
+    chessArr1.append([])
+    for j in range(11):
+        chessArr1[i].append(0)
+        
+# 方式二:
+lst = [([0] * 3) for i in range(4)]
