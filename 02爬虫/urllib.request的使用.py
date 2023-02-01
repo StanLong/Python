2ea8f1f0b7775a7105cc80b142e9fc006f33a3f4
StanLong <youlinbhz@sina.cn>
2023-02-01 21:34:03 +0800

    爬虫

--- 02爬虫/urllib.request的使用.py
+++ 02爬虫/urllib.request的使用.py
@@ -1,15 +1,14 @@
-# urllib基本使用
+# urllib基本使用 - 下载
 import urllib.request
 
-# 1. 定义一个url, 就是要访问的地址
-url = "http://www.baidu.com"
+# 下载网页
+# url_page = 'http://www.baidu.com'
+# urllib.request.urlretrieve(url_page, 'baidu.html') # 参数是是下载路径，参数2是给下载的文件起个名字
 
-# 2. 模拟浏览器向服务器发送请求并接收响应
-reponse = urllib.request.urlopen(url)
+# 下载图片
+# url_img = 'https://nimg.ws.126.net/?url=https://dingyue.ws.126.net/2021/0329/8ad4eca3j00qqq7cr0064c000u000tim.jpg&thumbnail=650x2147483647&quality=80&type=jpg'
+# urllib.request.urlretrieve(url=url_img, filename='jieqi.jpg')
 
-# 3. 获取响应页面中的源码
-# read() 返回的是字节形式的二进制数据，需要将二进制的数据转换为字符串，即解码 decode('编码格式')
-content = reponse.read().decode('utf-8')
-
-# 4. 打印结果
-print(content)
+# 下载视频
+url_video = 'https://vd3.bdstatic.com/mda-mhkku4ndaka5etk3/sc/cae_h264/1629557146440689988/mda-mhkku4ndaka5etk3.mp4?v_from_s=hkapp-haokan-nanjing&amp;auth_key=1675259913-0-0-5f767ca337e72bc7ed98ace1397f0070&amp;bcevod_channel=searchbox_feed&amp;cd=0&amp;pd=1&amp;pt=3&amp;logid=1712979472&amp;vid=7322829317245497064&amp;abtest=106847_1&amp;klogid=1712979472'
+urllib.request.urlretrieve(url=url_video, filename='haokan.mp4')
